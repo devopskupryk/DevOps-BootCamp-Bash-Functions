0264f0cfb54bddc9050ce09148c7688f1606213e
Siarhei Kupryk <siarhei.kupryk@itdep.net>
2022-12-15 15:49:40 +0300

    Version 01

---
+++ check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source ./functions.sh
+
+# pow 2 3
+# echo $result
+# pow 2 5
+# echo $result
+# pow 0 3
+# echo $result
+# pow 1 1
+# echo $result
+# pow 10 5
+# echo $result
+
+# shortest "Java" "Bash" "Python"
+# echo -e $result 
+# shortest "Oh" "this" "is" "a" "test" "!"
+# echo -e $result
+# shortest "This" "is" "Bash" "Functions" "Task"
+# echo -e $result
+# shortest "1" "2" "42"
+# echo -e $result
+# shortest "Test" "is" "short" "do"
+# echo -e $result
+
+#print_log "Hello World!"
+#echo -e $result
--- functions.sh
+++ functions.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-# Put your code here
+function pow () {
+    result=$(($1**$2))
+}
+
+function shortest () {
+    args=($*)
+    #echo ${args[*]}
+    for ((i=1;i<$#;i++)); do
+        for ((j=0;j<$#;j++)); do
+            if ((${#args[j]}>${#args[i]})); then
+                temp=${args[i]}
+                args[i]=${args[j]}
+                args[j]=$temp
+            fi
+        done
+    done
+    #echo ${args[*]}
+    result="${args[0]}\n"
+    for ((i=0;i<$#;i++)); do
+        if ((${#args[i+1]}==${#args[0]})); then
+            result="$result${args[i+1]}\n"
+        fi        
+    done
+}
+
+print_log() {
+    result="[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M)] $1"
+}
